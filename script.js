61602ee67c40acd1d519be1dc6bf7a3298e00e4c
iyasere <133278603+iyaseree@users.noreply.github.com>
2023-07-13 03:16:57 +0100

    adding new files

---
+++ script.js
@@ -0,0 +1,19 @@
+let user = [ {
+   username: 'kate',
+   age: 31,
+   timeline: 'writing exams'
+},
+  {
+   username: 'james',
+   age: 39,
+   timeline: 'going groceries shopping'
+  },
+   {
+   username: 'manny',
+   age: 39,
+   timeline: 'kite is so fun'
+  },
+]
+
+let username = user[2]?.username
+console.log(username) 
